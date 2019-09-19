3fbefed0f3e7ab946869fd4de671c1a9af200deb
Eduard Isaac <eduardizac@gmail.com>
2019-09-18 21:33:37 -0400

    segundo cambio

---
+++ index.php
@@ -0,0 +1,41 @@
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
+  </head>
+  <body>
+    
+  <?php
+    echo "<br></br>";
+  echo  "<h1>Eduard Isaac Frias Garcia</h1>" ;
+  echo "<br></br>";
+  echo " <h2> Mi Experiencia Como Informatico </h2> ";
+
+  echo  "<p>Me introduje al mundo de la informatica a la edad de 12 años cuando mi madre me habia regalado una computadora de mesa
+         con el objetivo de adquirid los conocimientos necesarios respecto a la tecnologia, no era muy moderna, recuerdo que era una compaq pentium 3 con 512mb de memoria y un disco duro de 40gb
+         para ese entonces, esa computadora era bastante buena, comence a aprender a utilizar el sistema operativo de windows, comandos, atajos y el funcionamiento de algunos archivos
+         luego el funcionamiento del hadware, hice un curso de reparacion de computadoras en el 2007, y comence a desarmar una y otra vez aquella vieja pc hasta que se daño, luego compre otra y fui practicando reparando computadoras de mis amigos
+         hasta llegar al punto de ser un soporte tecnico. en el 2010 comence a trabajar en una empresa de soporte tenico y fui adquiriendo conocimiento de redes y seguridad informatica, en el 2013 despues de haber tenido un año en la univerdad
+         aprendi logica de programacion y comence a hacer mini programas para mis amigos hasta que comence a trabajar directamente en el area de soporte tecnico y fui perdiendo la destreza en la programacion,
+         actualmente trabajo en san pedro de macoris, en papeleria cactus, como encargado del area de computo, resolviendo problemas internos de la impresa referente a sistemas y dando soporte en las ventas de los equipos electronicos </p> ";
+         echo "<br></br>";
+  
+
+  echo " <h3> MIS HOBBIES </h3> ";
+  echo "<p> como todo informatico que se respeta, me gustan las cosas Frikis o Geek, uno de mis pasatiempos favoritos es ver anime y jugar video juegos
+            tocar guitarra y dibujar, ver peliculas espeficiamente del area de la ficcion, terror, futuristas, superheroes, etc.
+            entre mis pasatiempos mas sanos e intructivos son, lee la biblia  y articulos o revistas enfocadas a la informatica y las nuevas tendencias </p>";
+
+
+  echo "<br></br>";
+  echo "<br></br>";
+
+
+    ?>
+  </body>
+  </html>
